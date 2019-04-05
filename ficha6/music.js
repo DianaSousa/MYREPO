313b66338603c9c535a55952fa05f692f2a54cb1
Diana Sousa <9180521@esmad.ipp.pt>
2019-04-05 16:31:24 +0100

    tá tranquilo

---
+++ ficha6/music.js
@@ -0,0 +1,13 @@
+class band{
+    constructor (name,genre,creationDate,origin,nMembers,photo,info){
+        this.name=name
+        this.genre=genre
+        this.creationDate=creationDate
+        this.origin=origin
+        this.nMembers=nMembers
+        this.photo=photo
+        this.info=info
+    }
+}
+const myBand= new band()
+const myBand2= new band()
